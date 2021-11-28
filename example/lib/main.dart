9bb4604ff0e76cd856c6b423d654ef00aeca6da0
Arash Mohammadi <arash13131313@gmail.com>
2021-11-28 11:13:43 +0330

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:dual_list_box/dual_list_box.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dual List Box',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(title: 'Dual List Box'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const DualListBox(),
+    );
+  }
+}
---
+++ lib/dual_list_box.dart
@@ -0,0 +1,1 @@
+export 'src/DualListBox.dart';
